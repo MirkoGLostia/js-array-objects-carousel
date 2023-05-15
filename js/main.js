14166dfaffe0b639a05e4b8211fb280626a25c4c
MirkoGLostia <giggi.torre@mail.com>
2023-05-15 14:57:32 +0200

    carousel-adapted

--- js/main.js
+++ js/main.js
@@ -2,7 +2,29 @@
 
 let imagePosition = 0;
 
-const image = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"]
+const images = [
+    {
+        image: 'img/01.webp',
+        title: "Marvel's Spiderman Miles Morale",
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+    }
+];
 
 const btnPrevious = document.getElementById("previous-image");
 
@@ -12,29 +34,26 @@ const btnNext = document.getElementById("next-image");
 
 // creazione container immagini principali e side
 
-for (let i = 0; i < image.length; i++) {
-    
-    const cicleImage = image[i];
+images.forEach ( (element) => {
 
-    // principali
-    const div = document.createElement('div');
-
-    div.innerHTML = `<img src="img/${cicleImage}" alt="immagine">`; 
-
-    document.getElementById("container").append(div);
-
-    // side
-    const divSecond = document.createElement('div');
-
-    divSecond.innerHTML = 
+    document.getElementById("container").innerHTML += `
+    <div style="background-image: url('${element.image}')">
+    <h2> ${element.title} </h2>
+    <p> ${element.text} </p>
+    </div>
     `
+
+    document.getElementById("side-bar-container").innerHTML += `
+    <div class="opacity-controller">
     <div class="opacity"></div>
-    <img src="img/${cicleImage}" alt="immagine">
-    `; 
-    
-    document.getElementById("side-bar-container").append(divSecond);
-    
-}
+    <img src="${element.image}" alt="immagine">
+    </div>
+    `
+
+    console.log(element);
+
+    } 
+);
 
 
 
@@ -47,7 +66,7 @@ for (let i = 0; i < image.length; i++) {
 
 
     // side images
-    const containerSideImage = document.querySelectorAll("#side-bar-container div div");
+    const containerSideImage = document.querySelectorAll(".opacity-controller div");
 
     console.log(containerSideImage);
 
